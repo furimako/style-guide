82d30ce174df3f329eedcdccd5f9c806fc2eeee8
furimako <furimako@gmail.com>
2018-12-24 20:39:09 +0900

    update git config

--- configs/git.sh
+++ configs/git.sh
@@ -1,4 +1,4 @@
 git config --system alias.co checkout
 git config --system alias.br branch
-git config --system alias.cm commit
+git config --system alias.cm 'commit -m'
 git config --system alias.st status
