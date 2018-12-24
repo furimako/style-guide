076f0001751e80f7ac76299152802cc20c27aa34
furimako <furimako@gmail.com>
2018-12-24 21:13:38 +0900

    update git config (add sudo)

--- configs/git.sh
+++ configs/git.sh
@@ -1,4 +1,4 @@
-git config --system alias.co checkout
-git config --system alias.br branch
-git config --system alias.cm 'commit -m'
-git config --system alias.st status
+sudo git config --system alias.co checkout
+sudo git config --system alias.br branch
+sudo git config --system alias.cm 'commit -m'
+sudo git config --system alias.st status
